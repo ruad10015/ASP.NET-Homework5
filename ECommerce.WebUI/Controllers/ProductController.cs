c818c189337650e6253047d84466f323fb085225
Dunya_sy83 <Dunya_sy83@itstep.edu.az>
2024-08-28 21:29:29 +0400

    Add project files.

---
+++ ECommerce.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,117 @@
+﻿using ECommerce.Business.Abstract;
+using ECommerce.WebUI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        public string Sort { get; set; } = "a-z";
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        // GET: ProductController
+        
+        public async Task<ActionResult> Index(int page=1,int category=0,string sort="")
+        {
+            int pageSize = 10;
+            var items=await _productService.GetAllByCategoryAsync(category);
+            var products = items.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+            if (sort == "a-z")
+            {
+                Sort = "z-a";
+                products = products.OrderBy(p => p.ProductName).ToList();
+            }
+            else if(sort == "z-a")
+            {
+                Sort = "a-z";
+                products = products.OrderByDescending(p => p.ProductName).ToList();
+            }
+
+            var model = new ProductListViewModel
+            {
+                Products = products,
+                CurrentCategory = category,
+                PageCount = (int)Math.Ceiling(items.Count / (double)pageSize),
+                PageSize = pageSize,
+                CurrentPage = page,
+                SortProduct = Sort,
+
+            };
+            return View(model);
+        }
+
+        // GET: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ECommerce.WebUI/Models/ProductListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ECommerce.Entities.Models;
+
+namespace ECommerce.WebUI.Models
+{
+    public class ProductListViewModel
+    {
+        public List<Product>? Products { get; set; }
+        public int CurrentCategory { get; internal set; }
+        public int PageCount { get; internal set; }
+        public int PageSize { get; internal set; }
+        public int CurrentPage { get; internal set; }
+        public string SortProduct { get; internal set; }
+    }
+}
